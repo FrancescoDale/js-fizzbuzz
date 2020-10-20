8262a80954ce8c221ebe69d4f590a712a1646668
FrancescoDale <fd060862@gmail.com>
2020-10-20 14:35:53 +0200

    fix nel ciclo for, cambiato operatore < in <=
    Inserita stampa di fizz dopo ogni numero divisibile per 3

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 // creo un ciclo che mi stampa i numeri da 1 a 100
 
-for (var i = 1; i < 101; i++) {
+for (var i = 1; i <= 100; i++) {
     console.log(i);
+    if (i % 3 === 0) {
+        console.log('Fizz');
+    }
 }
