e9492ab5fdafe47c2b325c169001a057a63196a8
FrancescoDale <fd060862@gmail.com>
2020-10-20 14:41:04 +0200

    aggiunta la condizione che se i è divisibile per 3 e 5 ( io ho preso il primo minimo comune multiplo, 15 ) stampa fizzbuzz dopo il numero

--- main.js
+++ main.js
@@ -8,4 +8,10 @@ for (var i = 1; i <= 100; i++) {
     else if (i % 5 === 0) {
         console.log('Buzz');
     }
+    else if (i % 15 === 0) {
+        console.log('FizzBuzz');
+    }
+    else {
+        console.log(i);
+    }
 }
